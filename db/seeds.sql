aa0696434b7627a045d35cf121367ef2fedcbc74
Timothy Peterson <fridayfred3p@gmail.com>
2020-10-11 11:00:02 -0700

    added seeds file with 3 burger examples

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Bacon Ranch', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Bypass Burger', false)
