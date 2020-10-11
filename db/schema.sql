c529fbc14790b4241ec88e2cedfe881ac81b1d16
Timothy Peterson <fridayfred3p@gmail.com>
2020-10-11 10:47:58 -0700

    added db folder and schema file with burgers_db created and burgers table created

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+Use burgers_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255),
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
